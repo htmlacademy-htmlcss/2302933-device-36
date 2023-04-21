a29902d8368e504c1b06318013bd9868754e0711
Petr <petmeln04@gmail.com>
2023-04-21 20:20:47 +0300

    Иправляет ссылку и вносит небольшие исправления

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ const advantagesButtons = document.querySelectorAll('.advantages-button');
 
 advantagesButtons.forEach((advantagesButton, index) => {
   advantagesButton.addEventListener('click', () => {
-    document.querySelector('.advantages-button.active').classList.remove('active');
-    advantagesButton.classList.add('active');
+    document.querySelector('.advantages-button-active').classList.remove('advantages-button-active');
+    advantagesButton.classList.add('advantages-button-active');
     document.querySelector('.advantages-content-item.active').classList.remove('active');
     advantages[index].classList.add('active');
   });
