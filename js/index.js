e85a81ca2548b7a1e10517e17a12bcaad8f969d4
Petr <petmeln04@gmail.com>
2023-04-24 22:23:20 +0300

    Исправляет слайды и добавляет возможность переключения их

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ advantagesButtons.forEach((advantagesButton, index) => {
   });
 });
 
+// МОДАЛЬНОЕ ОКНО
 
 const modalOpen = document.querySelector('.advantages-delivery-button');
 const modalClose = document.querySelector('.modal-close-button');
@@ -21,4 +22,48 @@ modalOpen.addEventListener('click', () => {
 
 modalClose.addEventListener('click', () => {
   modal.classList.add('hidden');
+});
+
+// СЛАЙДЕР
+
+const slidesTabButtons = document.querySelectorAll('.slide-pagination-button');
+const slides = document.querySelectorAll('.slide');
+const buttonPrevious = document.querySelector('.slide-previous');
+const buttonNext = document.querySelector('.slide-next');
+let counter = 0;
+
+const clearClasses = () => {
+  document.querySelector('.slide-current').classList.remove('slide-current');
+  document.querySelector('.slide-pagination-button-active').classList.remove('slide-pagination-button-active');
+};
+
+slidesTabButtons.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    clearClasses();
+    counter = index;
+    slides[index].classList.add('slide-current');
+    tab.classList.add('slide-pagination-button-active');
+  });
+});
+
+buttonPrevious.addEventListener('click', () => {
+  clearClasses();
+  counter = counter - 1;
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  slides[counter].classList.add('slide-current');
+  slidesTabButtons[counter].classList.add('slide-pagination-button-active');
+});
+
+buttonNext.addEventListener('click', () => {
+  clearClasses();
+  counter = counter + 1;
+  if (counter > slides.length - 1) {
+    counter = 0;
+  }
+
+  slides[counter].classList.add('slide-current');
+  slidesTabButtons[counter].classList.add('slide-pagination-button-active');
 });
